70e4c889956bb750e214d10bb84720ce20820cf4
Hector Ramon <hlramon93@gmail.com>
2025-04-23 19:37:16 -0400

    Add template

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,11 +1,12 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Tab2Page } from "./tab2.page";
+import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { Tab2PageRoutingModule } from "./tab2-routing.module";
+import { LoginComponent } from "../auth/login/login.component";
 
 @NgModule({
   imports: [
@@ -13,8 +14,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page, LoginComponent],
 })
 export class Tab2PageModule {}
