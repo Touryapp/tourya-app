3084f4de4d203792e13482afba49bf5dad8ab536
Hector Ramon <hlramon93@gmail.com>
2025-04-29 18:59:32 -0400

    Add US01

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,21 +1,20 @@
 import { IonicModule } from "@ionic/angular";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
 import { Tab1Page } from "./tab1.page";
 import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 
 import { Tab1PageRoutingModule } from "./tab1-routing.module";
-import { RegisterComponent } from "../auth/register/register.component";
+import { SharedModule } from "../shared/shared-module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
+    SharedModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page, RegisterComponent],
+  declarations: [Tab1Page],
 })
 export class Tab1PageModule {}
